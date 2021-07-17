0986e5a531aecb90e38d90d643833d563e558a97
Cijomanuel <44916914+Cijomanuel@users.noreply.github.com>
2021-07-17 18:57:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,149 @@
+
+function info(message) {
+    let msg = parseInt(message)
+    if (msg === 1) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_1').style.display = "flex";
+
+    } else if (msg === 2) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_2').style.display = "flex";
+
+    } else if (msg === 3) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_3').style.display = "flex";
+
+    } else if (msg === 4) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_4').style.display = "flex";
+
+    } else if (msg === 5) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_5').style.display = "flex";
+
+    } else if (msg === 6) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_6').style.display = "flex";
+
+    } else if (msg === 7) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_7').style.display = "flex";
+
+    } else if (msg === 8) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_8').style.display = "flex";
+
+    } else if (msg === 9) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_9').style.display = "flex";
+
+    } else if (msg === 10) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_10').style.display = "flex";
+
+    } else if (msg === 11) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_11').style.display = "flex";
+
+    } else if (msg === 12) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_12').style.display = "flex";
+
+    } else if (msg === 13) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_13').style.display = "flex";
+
+    } else if (msg === 14) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_14').style.display = "flex";
+
+    } else if (msg === 15) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_15').style.display = "flex";
+
+    } else if (msg === 16) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_16').style.display = "flex";
+
+    } else if (msg === 17) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_17').style.display = "flex";
+
+    } else if (msg === 18) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_18').style.display = "flex";
+
+    } else if (msg === 19) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_19').style.display = "flex";
+
+    } else if (msg === 20) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_20').style.display = "flex";
+
+    } else if (msg === 21) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_21').style.display = "flex";
+
+    } else if (msg === 22) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_22').style.display = "flex";
+
+    } else if (msg === 23) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_23').style.display = "flex";
+
+    } else if (msg === 24) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_24').style.display = "flex";
+
+    } else if (msg === 25) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_25').style.display = "flex";
+
+    } else if (msg === 26) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_26').style.display = "flex";
+
+    } else if (msg === 27) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_27').style.display = "flex";
+
+    } else if (msg === 28) {
+        document.getElementById('content').style.display = "none";
+        document.getElementById('content_28').style.display = "flex";
+
+    } else {
+
+        document.getElementById('content').style.display = "flex";
+        document.getElementById('content_1').style.display = "none";
+        document.getElementById('content_2').style.display = "none";
+        document.getElementById('content_3').style.display = "none";
+        document.getElementById('content_4').style.display = "none";
+        document.getElementById('content_5').style.display = "none";
+        document.getElementById('content_6').style.display = "none";
+        document.getElementById('content_7').style.display = "none";
+        document.getElementById('content_8').style.display = "none";
+        document.getElementById('content_9').style.display = "none";
+        document.getElementById('content_10').style.display = "none";
+        document.getElementById('content_11').style.display = "none";
+        document.getElementById('content_12').style.display = "none";
+        document.getElementById('content_13').style.display = "none";
+        document.getElementById('content_14').style.display = "none";
+        document.getElementById('content_15').style.display = "none";
+        document.getElementById('content_16').style.display = "none";
+        document.getElementById('content_17').style.display = "none";
+        document.getElementById('content_18').style.display = "none";
+        document.getElementById('content_19').style.display = "none";
+        document.getElementById('content_20').style.display = "none";
+        document.getElementById('content_21').style.display = "none";
+        document.getElementById('content_22').style.display = "none";
+        document.getElementById('content_23').style.display = "none";
+        document.getElementById('content_24').style.display = "none";
+        document.getElementById('content_25').style.display = "none";
+        document.getElementById('content_26').style.display = "none";
+        document.getElementById('content_27').style.display = "none";
+        document.getElementById('content_28').style.display = "none";
+    }
+
+}
